e268b6258e72d25285db5f5ab4e66050ed1fc356
Jonah Andersson <cjonah@hotmail.se>
2022-08-28 17:53:59 +0200

    Added logic on the service bus sender example

---
+++ AzureServiceBus.KeyVaultService/KeyVaultService.cs
@@ -0,0 +1,30 @@
+﻿
+using Microsoft.Extensions.Configuration;
+
+namespace AzureServiceBus.Services
+{
+    public class KeyVaultService
+    {
+
+       
+
+       //SecretClientOptions options = new SecretClientOptions()
+        //{
+        //    Retry =
+        //         {
+        //            Delay= TimeSpan.FromSeconds(2),
+        //            MaxDelay = TimeSpan.FromSeconds(16),
+        //            MaxRetries = 5,
+        //            Mode = RetryMode.Exponential
+        //         }
+        //};
+
+        //var client = new SecretClient(new Uri("https://<your-unique-key-vault-name>.vault.azure.net/"), new DefaultAzureCredential(), options);
+
+        //KeyVaultSecret secret = client.GetSecret("<mySecret>");
+
+        //string secretValue = secret.Value;
+
+
+    }
+}
--- AzureServiceBus.Queue.Sender/Program.cs
+++ AzureServiceBus.Queue.Sender/Program.cs
@@ -1,10 +1,76 @@
-﻿namespace AzureServiceBus.Queue.Sender
+﻿using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using AzureServiceBus.Services;
+using Azure.Messaging.ServiceBus;
+
+namespace AzureServiceBus.Queue.Sender
 {
-    internal class Program
+
+    public class Program
     {
-        static void Main(string[] args)
+        static async Task Main()
         {
-            Console.WriteLine("Hello, World!");
+
+            // Build a config object, using env vars and JSON providers.
+            var config = new ConfigurationBuilder()
+                        .AddJsonFile("appsettings.json")
+                        .AddEnvironmentVariables()
+                        .Build();
+
+            //TODO: Use KeyVault and setup service
+            //KeyVaultSettings keyVaultSettings = config.GetRequiredSection("KeyVaultSettings").Get<KeyVaultSettings>();
+
+            ServiceBusSettings serviceBusSettings = config.GetRequiredSection("ServiceBusSettings").Get<ServiceBusSettings>();
+            var connectionString = serviceBusSettings.ServiceBusNameSpaceConnectionString;
+            var queueName = serviceBusSettings.ServiceBusQueueName;
+
+            ServiceBusClient client;
+            ServiceBusSender sender;
+
+            // number of messages to be sent to the queue
+            const int numOfMessages = 3;
+
+
+            // The Service Bus client types are safe to cache and use as a singleton for the lifetime
+            // of the application, which is best practice when messages are being published or read
+            // regularly. Set the transport type to AmqpWebSockets so that the ServiceBusClient uses the port 443. 
+            // If you use the default AmqpTcp, you will need to make sure that the ports 5671 and 5672 are open
+
+            var clientOptions = new ServiceBusClientOptions() { TransportType = ServiceBusTransportType.AmqpWebSockets };
+            client = new ServiceBusClient(connectionString, clientOptions);
+            sender = client.CreateSender(queueName);
+
+            // create a batch 
+            using ServiceBusMessageBatch messageBatch = await sender.CreateMessageBatchAsync();
+
+            for (int i = 1; i <= numOfMessages; i++)
+            {
+                // try adding a message to the batch
+                if (!messageBatch.TryAddMessage(new ServiceBusMessage($"Message {i}")))
+                {
+                    // if it is too large for the batch
+                    throw new Exception($"The message {i} is too large to fit in the batch.");
+                }
+            }
+
+            try
+            {
+                // Use the producer client to send the batch of messages to the Service Bus queue
+                await sender.SendMessagesAsync(messageBatch);
+                Console.WriteLine($"A batch of {numOfMessages} messages has been published to the queue.");
+            }
+            finally
+            {
+                // Calling DisposeAsync on client types is required to ensure that network
+                // resources and other unmanaged objects are properly cleaned up.
+                await sender.DisposeAsync();
+                await client.DisposeAsync();
+            }
+
+            Console.WriteLine("Press any key to end the application");
+            Console.ReadKey();
         }
     }
 }
